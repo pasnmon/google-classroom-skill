48f09f41775128ecb746ff5ef2097d26050a36a8
Pascal Starkl <pascal@starkl.de>
2020-05-25 13:29:50 +0200

    Added dialog and intents for new and due courses/course works

--- __init__.py
+++ __init__.py
@@ -1,11 +1,16 @@
-from mycroft import MycroftSkill, intent_file_handler
+from datetime import datetime, date, timedelta
+
 import pickle
 import os.path
+
+# Mycroft
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_file_handler
+
+# Google
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from datetime import datetime, date, timedelta
-import inflect
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly',
@@ -32,39 +37,29 @@ def create_auth():
             pickle.dump(creds, token)
 
 
-def get_due_date(due_date, due_time):
+def get_date(due_date):
     if not due_date:
         return False
-    if due_date and due_time.get("hours") and due_time.get("minutes"):
-        datetimeString = "{}-{}-{}T{}:{}".format(
-            due_date.get("year"),
-            due_date.get("month"),
-            due_date.get("day"),
-            due_time.get("hours"),
-            due_time.get("minutes")
-        )
-        return datetime.strptime(datetimeString, "%Y-%m-%dT%H:%M")
-
-    datetimeString = "{}-{}-{}".format(
+    datetime_string = "{}-{}-{}".format(
         due_date.get("year"),
         due_date.get("month"),
         due_date.get("day"),
     )
-    return datetime.strptime(datetimeString, "%Y-%m-%d")
+    return datetime.strptime(datetime_string, "%Y-%m-%d")
 
-def build_date(day,day_num,month,year):
-    sug_date = None
-    if day:
-        if day == "today":
-            sug_date = date.today()
-        elif day == "yesterday":
-            sug_date = date.today() - timedelta(days=1)
-        elif day == "tomorrow":
-            sug_date = date.today() + timedelta(days=1)
-        return sug_date
-    if day_num and month:
-        inf = inflect.engine()
-        num = inf.
+
+def build_date(day):
+    """
+    Returns the date object of the string
+    :param day: String
+    :return date object
+    """
+    if day == "tomorrow":
+        return  date.today() + timedelta(days=1)
+    if day == "yesterday":
+        return date.today() - timedelta(days=1)
+    return date.today()
+
 
 class GoogleClassroomSkill(MycroftSkill):
     def __init__(self):
@@ -92,19 +87,34 @@ class GoogleClassroomSkill(MycroftSkill):
         return self.connection
 
     @intent_file_handler('google.classroom.news.intent')
-    def handle_classroom_google(self, message):
-        self.speak_dialog('classroom.google')
-
-    @intent_file_handler('google.classroom.due.intent')
-    def handel_due_intent(self, message, connection):
+    def handle_news_intent(self, message):
         courses = self.connection.courses.list().execute()
-        day = message.data.get('day')
-        year = message.data.get('year')
-        day_num = message.data.get('day_num')
-        month = message.data.get('month')
+        day = date.today()
+        new_course = []
+        new_cw = []
+        for course in courses:
+            if day == get_date(course.get("createDay")):
+                new_course.append(course)
+            course_works = self.connection.courses().courseWork().list(courseId=course.google_id).execute()
+            new_cw += [cw for cw in course_works if day == get_date(cw.get("createDate"))]
+        self.speak_dialog('google.classroom.news.dialog', {
+            'number': len(new_course) or 'no', 'number': len(new_cw) or 'no'
+        })
+
+    @intent_file_handler(IntentBuilder('google.classroom.due.intent').require('day'))
+    def handel_due_intent(self, message):
+        courses = self.connection.courses.list().execute()
+        day_str = message.data.get('day')
+        day = build_date(day_str)
+        cw_to_notify = []
         for course in courses:
             course_works = self.connection.courses().courseWork().list(courseId=course.google_id).execute()
-            for cw in course_works:
+            cw_to_notify += [cw for cw in course_works if day == get_date(cw.get("dueDate"))]
+        if cw_to_notify:
+            self.speak_dialog('google.classroom.due.dialog', {'day': day_str, 'number': len(cw_to_notify)})
+        else:
+            self.speak_dialog('google.classroom.nothing.due.dialog', {'day': day_str})
+
 
 def create_skill():
     return GoogleClassroomSkill()
